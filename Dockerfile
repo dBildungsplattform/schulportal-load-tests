41f82521ef519001df97a0ca30160d1e475595a7
aimee-889 <aimee.weiss@capgemini.com>
2024-10-15 14:24:52 +0200

    test it

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,5 @@
 FROM grafana/k6:0.54.0
 USER root
 
-COPY wrapper-script.sh wrapper-script.sh
 RUN apk add git && \
     chmod +x wrapper-script.sh
-
-ENTRYPOINT ["./wrapper-script.sh"]
--- run.sh
+++ run.sh
@@ -9,18 +9,25 @@ if [[ -z "$PATTERN" ]]; then
     PATTERN="*"
 fi
 
-# KC_BASE=$2 # not needed yet
-
+# only for local development
 SKIP_SSL=""
 if [[ "$SPSH_BASE" =~ "localhost" ]]; then
     SKIP_SSL="--insecure-skip-tls-verify"
 fi
 
+# create output/, if not present
+if [[ ! -d output/ ]]; then
+    mkdir output/
+fi
+
+
+# einmal Grafana ohne out mit der CSV Datei ()
+# funktinonieren die Ergebnisse in den Logs auch wenn die Parallel laufen? 
 for uc in loadtest/usecases/*; do
     if [[ "$uc" =~ "$PATTERN" ]]; then
         filename=${uc##*/}
         csv="output/${filename%.ts}.csv"
         touch "$csv"
-        k6 run --compatibility-mode=experimental_enhanced --out csv="$csv" "$SKIP_SSL" -e SPSH_BASE="$SPSH_BASE" -e CONFIG="$CONFIG" -e KC_BASE="$KC_BASE" "$uc"
+        k6 run --compatibility-mode=experimental_enhanced --out csv="$csv" "$SKIP_SSL" -e SPSH_BASE="$SPSH_BASE" -e CONFIG="$CONFIG" "$uc"
     fi
 done
