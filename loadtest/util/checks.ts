a7542f8bab78063e98df6acef040ca209df59bbc
Yannick Clausen <yannick.clausen@capgemini.com>
2024-11-25 11:52:44 +0100

    reduce to 1 timing check

--- loadtest/util/checks.ts
+++ loadtest/util/checks.ts
@@ -15,11 +15,7 @@ export function getTimeChecker(maxTime: number) {
 }
 
 export const defaultTimingCheck = {
-  "took <250ms": getTimeChecker(250),
-  "took <500ms": getTimeChecker(500),
   "took <1000ms": getTimeChecker(1000),
-  "took <2000ms": getTimeChecker(2000),
-  "took <5000ms": getTimeChecker(5000),
 };
 
 export const defaultHttpCheck = {
